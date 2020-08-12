ab8fc9f65489ab185e8d138e55deb03015ec3e00
Jorge Attie <attiejorge89@gmail.com>
2020-08-12 17:13:04 -0300

    dd2

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors'
 import routes from './routes';
 
+
+const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cors());
 app.use(express.json())
 
 app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
 
